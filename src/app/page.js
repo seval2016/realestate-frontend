79c0cb76e75579494a41aed39badadb1fccb7aa7
Ayşenur Dinç <nurayse89@hotmail.com>
2024-08-18 12:48:00 +0300

    ayse

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener noreferrerr"
           >
             By{" "}
             <Image
